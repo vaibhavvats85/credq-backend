ccd1004c4958273a1dc10a7e7fe2b0e981ed3d84
vaibhavvats85 <vaibhavvats85@gmail.com>
2021-08-15 15:50:46 +0530

    return statement updated for email

--- controllers/email.js
+++ controllers/email.js
@@ -3,6 +3,6 @@ const { sendMail2Requestor } = require("../utils/email");
 exports.email = async (req, res) => {
   const user = req.body;
   const def = await sendMail2Requestor("info@credq.org", user);
-  if (def === "Successful!") res.status(200).send(def);
-  res.status(400).send(def);
+  if (def === "Successful!") return  res.status(200).send(def);
+  return res.status(400).send(def);
 };
